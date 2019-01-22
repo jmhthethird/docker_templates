f2aa4a354b16fe7aae31a5718cf93222cabf2d26
Jack Hefner III <jmhthethird@gmail.com>
2019-01-21 20:42:22 -0800

    copy env file and source it during container run. echo vars so you can see that they are loaded in the running container.

---
+++ copy_env_file_and_source_it_during_image_run/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source .env
+
+echo Aperture: ${APERTURE}
+echo Weyland: ${WEYLAND_CORPORATION}
+echo Veridian Dynamics: ${VERIDIAN_DYNAMICS}
+echo Umbrella Corp: ${UMBRELLA_CORPORATION}
+echo Tyrell Corporation: ${TYRELL_CORPORATION}
+echo BRAWNDO!: ${BRAWNDO}
