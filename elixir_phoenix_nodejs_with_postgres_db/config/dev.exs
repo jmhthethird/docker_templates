6e7ee35c933db7cb8a1bf971d7f5d1c9f4282679
Jack Hefner III <jmhthethird@gmail.com>
2019-01-28 21:44:56 -0800

    move db config to env vars (best practice) and update run.sh

--- elixir_phoenix_nodejs_with_postgres_db/config/dev.exs
+++ elixir_phoenix_nodejs_with_postgres_db/config/dev.exs
@@ -68,8 +68,8 @@ config :phoenix, :plug_init_mode, :runtime
 
 # Configure your database
 config :test_app, TestApp.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "test_app_dev",
-  hostname: "localhost",
+  username: System.get_env("PGUSER"),
+  password: System.get_env("PGPASSWORD"),
+  database: System.get_env("PGDATABASE"),
+  hostname: System.get_env("PGHOST"),
   pool_size: 10
--- elixir_phoenix_nodejs_with_postgres_db/run.sh
+++ elixir_phoenix_nodejs_with_postgres_db/run.sh
@@ -3,7 +3,22 @@
 # -e Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Get Phoenix application ready for startup.
-mix deps.get
+# Set postgres env vars
+export PGUSER="postgres"
+export PGPASSWORD="postgres"
+export PGDATABASE="test_app"
+export PGHOST="db"
+
+# Connect and create db for app
+mix ecto.create
+
+# Run any migrations for app
+mix ecto.migrate
+
+
+# Get Phoenix assets ready for startup.
+mix do deps.get, deps.compile
 cd assets && npm install
 cd ..
+
+iex -S mix phx.server
